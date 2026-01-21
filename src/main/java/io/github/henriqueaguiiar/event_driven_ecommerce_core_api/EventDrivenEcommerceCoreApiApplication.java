fc1b5743a6c8601bb8b505b0f60cb93a963693d8
Henrique Pacheco <DTI.Henrique.Pacheco@segurossura.com.br>
2026-01-21 16:05:10 -0300

    initial commit

---
+++ src/main/java/io/github/henriqueaguiiar/event_driven_ecommerce_core_api/EventDrivenEcommerceCoreApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.henriqueaguiiar.event_driven_ecommerce_core_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventDrivenEcommerceCoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventDrivenEcommerceCoreApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/henriqueaguiiar/event_driven_ecommerce_core_api/EventDrivenEcommerceCoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.henriqueaguiiar.event_driven_ecommerce_core_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventDrivenEcommerceCoreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
